5fb595fd14e30c97f8e2ef8415434508b8e5a879
Miguel <miguelsilvapeloso@gmail.com>
2026-01-16 12:45:12 -0300

    Initial changes

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,97 @@
 
 set -ouex pipefail
 
-### Install packages
+### Cleanup from Zirconium
+dnf remove -y \ 
+	foot \
+	NetworkManager-adsl \
+  	NetworkManager-bluetooth \
+  	NetworkManager-config-connectivity-fedora \
+  	NetworkManager-libnm \
+  	NetworkManager-openconnect \
+  	NetworkManager-openvpn \
+  	NetworkManager-strongswan \
+  	NetworkManager-vpnc \
+  	NetworkManager-wifi \
+  	NetworkManager-wwan \
+  	audit \
+  	cups \
+  	cups-pk-helper \
+  	dymo-cups-drivers \
+  	hplip \
+  	hyperv-daemons \
+  	open-vm-tools \
+  	open-vm-tools-desktop \
+  	openconnect \
+  	pam_yubico \
+  	printer-driver-brlaser \
+  	ptouch-driver \
+  	qemu-guest-agent \
+  	spice-vdagent \
+  	steam-devices \
+  	switcheroo-control \
+  	system-config-printer-libs \
+  	system-config-printer-udev \
+  	uxplay \
+  	vpnc \
+  	whois \
+  	wireguard-tools \
+  	fcitx5-mozc \
+  	valent-git \
+  	input-remapper 
+  	
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+  	
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+### Install Kernel Cachyos (Stole From Piperita)
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
+  rpm --erase $pkg --nodeps
+done
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+pushd /usr/lib/kernel/install.d
+printf '%s\n' '#!/bin/sh' 'exit 0' > 05-rpmostree.install
+printf '%s\n' '#!/bin/sh' 'exit 0' > 50-dracut.install
+chmod +x  05-rpmostree.install 50-dracut.install
+popd
 
-#### Example for enabling a System Unit File
+dnf -y copr enable bieszczaders/kernel-cachyos-lto
+dnf -y copr disable bieszczaders/kernel-cachyos-lto
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto install \
+  kernel-cachyos-lto
 
-systemctl enable podman.socket
+dnf -y copr enable bieszczaders/kernel-cachyos-addons
+dnf -y copr disable bieszczaders/kernel-cachyos-addons
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons swap zram-generator-defaults cachyos-settings
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons install \
+  scx-scheds-git \
+  scx-manager
+  
+tee /usr/lib/modules-load.d/ntsync.conf <<'EOF'
+ntsync
+EOF
+
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+export DRACUT_NO_XATTR=1
+depmod -a "$(ls -1 /lib/modules/ | tail -1)"
+dracut --no-hostonly --kver "$KERNEL_VERSION" --reproducible --zstd -v --add ostree -f "/usr/lib/modules/$KERNEL_VERSION/initramfs.img"
+chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+
+### Install LACT
+dnf -y copr enable ilyaz/LACT
+dnf -y install lact
+systemctl enable lactd
+dnf -y copr disable ilyaz/LACT
+
+### Install Gpu Screen Recorder
+dnf -y copr enable brycensranch/gpu-screen-recorder-git
+dnf -y install gpu-screen-recorder-ui
+dnf -y copr disable brycensranch/gpu-screen-recorder-git
+
+### Install packages from repos
+dnf -y install \
+	kitty 	\
+	neovim   \
+	openrgb	  \
+	openrgb-udev-rules
+
+systemctl enable openrgb
